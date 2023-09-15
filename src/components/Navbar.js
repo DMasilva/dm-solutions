1e165976404f833ec9da334b46b1d99b9c056cb5
David <david.owuor@student.moringaschool.com>
2023-09-15 16:30:09 -0500

    navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,15 +11,15 @@ const Navbar = () => {
             </div>
             <div className="md:static absolute md:min-h-fit min-h-[60vh] left-0 top-[10%] md:w-auto w-full flex items-center px-5">
                 <ul className="flex md:flex-row flex-col md:items-center md:gap-[4vm] gap-8" >
-                    <li>test</li>
-                    <li>test</li>
-                    <li>test</li>
-                    <li>test</li>
-                    <li>test</li>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Services</li>
+                    <li>Testimonials</li>
+                    <li>Contacts</li>
                 </ul>
             </div>
             <div>
-                <button className="bg-blue-300 px-5 py-2 rounded-full hover:bg-black">Sign in</button>
+                <button onClick={()=>alert('test')} className="bg-blue-300 px-5 py-2 rounded-full hover:bg-black">Sign in</button>
             </div>
         </nav>
       </header>
