fce29fed2be17ecf36481299a9f1c5b5369dc3c8
David <david.owuor@student.moringaschool.com>
2023-09-21 11:09:11 -0500

    login updates

---
+++ src/components/Login.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div>
+        <form className='border-4 h-[400px] w-[300px] m-auto rounded-md items-center overflow-hidden'>
+            <div className='test'>
+            <label>username</label>
+            <input className='border-4 border-red-400' type='text'/>
+            </div>
+            <div>
+            <label>password</label>
+            <input type='password'/>
+            </div>
+            <div className='test2'>
+                <input type='submit' value='login'/>
+            </div>
+        </form>
+    </div>
+  )
+}
+
+export default Login
