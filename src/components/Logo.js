e522462504905339ce6d2b1e3eeb15e950fc0264
David <david.owuor@student.moringaschool.com>
2023-09-23 17:12:14 -0500

    changes in responsiveness

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -2,13 +2,13 @@ import React from "react";
 
 const Logo = () => {
   return (
-    <div>
-      <div className="text-center p-3 m-3 w-[200px]">
+      <div className="text-center sm:align-text-left">
+       <h1 className="text-center ml-2 md:text-left">
         <span className="text-red-500 text-2xl font-bold">DM</span>
         <span className="text-blue-500 text-2xl font-bold">S</span>
-        <span className="text-white text-bold ">olutions</span>
+        <span className="text-gray-900 text-xl invisible sm:visible text-bold ">olutions</span>
+        </h1>
       </div>
-    </div>
   );
 };
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,33 +3,27 @@ import Logo from "./Logo";
 
 const Navbar = () => {
   return (
-    <div className="">
-      <header>
-        <nav className="">
-          <div className=" bg-black md:grid grid-cols-3 sm:block">
-            <div className="border-2">
-              <Logo />
-            </div>
-            <div className="border-2">
-              <ul className="flex md:flex-row flex-col md:items-center md:gap-[4vm] gap-8">
-                <li>Home</li>
-                <li>About</li>
-                <li>Services</li>
-                <li>Testimonials</li>
-                <li>Contacts</li>
-              </ul>
-            </div>
-            <div className="flex justify-end sm:block border-2">
-              <button className="bg-blue-300 px-5 py-2 rounded-full hover:bg-black">
-                Sign up
-              </button>
-              <button className="bg-blue-300 px-5 py-2 rounded-full hover:bg-black">
-                Log in
-              </button>
-            </div>
-          </div>
-        </nav>
-      </header>
+    <div className="md:text-center">
+      <nav className="sm:flex">
+      <div className="mb-4 sm:mb-0">
+        <Logo/>
+      </div>
+
+      <div className="w-full">
+        <ul className="flex flex-col sm:flex-row sm:justify-center">
+         <li className="links"><a href='#'>Home</a></li>
+         <li className="links"><a href='#'>About</a></li>
+         <li className="links"><a href='#'>Services</a></li>
+         <li className="links"><a href='#'>Masilva</a></li>
+        </ul>
+      </div>
+      <div className="text-center">
+        <a className="m-1 bg-blue-500 rounded border-2" href='#'>Login</a>
+        <a className="m-1 bg-blue-500 rounded border-2 md:mr-4" href='#'>Signup</a>
+      </div>
+
+      </nav>
+
     </div>
   );
 };
