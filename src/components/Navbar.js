9c8f2386ed9bded03bb15ee444ebc2ee529a94a5
David <david.owuor@student.moringaschool.com>
2023-09-24 21:34:50 -0500

    more styls

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,14 @@
 import React from "react";
 import Logo from "./Logo";
+import { FaHome } from 'react-icons/fa';
+import {BsFillPatchQuestionFill} from 'react-icons/bs'
+import {MdOutlineMiscellaneousServices} from 'react-icons/md'
+import {CgProfile} from 'react-icons/cg'
 
 const Navbar = () => {
+
   return (
-    <div className="md:text-center">
+    <div className="md:text-center md:mx-2 mx-1 mt-2 md:text-xl bg-gradient-to-t from-blue-400 to-blue-100 p-2 sm:p-4">
       <nav className="sm:flex">
       <div className="mb-4 sm:mb-0">
         <Logo/>
@@ -11,19 +16,18 @@ const Navbar = () => {
 
       <div className="w-full">
         <ul className="flex flex-col sm:flex-row sm:justify-center">
-         <li className="links"><a href='#'>Home</a></li>
-         <li className="links"><a href='#'>About</a></li>
-         <li className="links"><a href='#'>Services</a></li>
-         <li className="links"><a href='#'>Masilva</a></li>
+        <li className="links"><a href='#'><span className="inline-block mx-1"> <FaHome /></span>Home</a></li>
+         <li className="links"><a href='#'><span className="inline-block mx-1"><BsFillPatchQuestionFill/></span>About</a></li>
+         <li className="links"><a href='#'><span className="inline-block mx-1"><MdOutlineMiscellaneousServices/></span>Services</a></li>
+         <li className="links"><a href='#'><span className="inline-block mx-1"><CgProfile/></span>Masilva</a></li>
         </ul>
       </div>
       <div className="text-center">
-        <a className="m-1 bg-blue-500 rounded border-2" href='#'>Login</a>
-        <a className="m-1 bg-blue-500 rounded border-2 md:mr-4" href='#'>Signup</a>
+        <a className="m-1 bg-white hover:bg-blue-500 rounded border-2" href='#'>Login</a>
+
+        <a className="m-1 bg-white hover:bg-blue-500 rounded border-2 md:mr-4" href='#'>Signup</a>
       </div>
-
       </nav>
-
     </div>
   );
 };
