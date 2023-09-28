83b79650f0f4b1f4a2e93c090ce510702bd275e1
David <david.owuor@student.moringaschool.com>
2023-09-27 22:12:43 -0500

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,31 +1,43 @@
-import React from 'react'
-import {BsWhatsapp} from 'react-icons/bs'
-import {BiLogoFacebookCircle} from 'react-icons/bi'
-import {BsLinkedin} from 'react-icons/bs'
-import {BsInstagram} from 'react-icons/bs'
-import {BsGithub} from 'react-icons/bs'
-import {AiOutlineCopyright} from 'react-icons/ai'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { BsWhatsapp } from "react-icons/bs";
+import { BiLogoFacebookCircle } from "react-icons/bi";
+import { BsLinkedin } from "react-icons/bs";
+import { BsInstagram } from "react-icons/bs";
+import { BsGithub } from "react-icons/bs";
+import { AiOutlineCopyright } from "react-icons/ai";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
-  const date = Date.now().toExponential()
+  const date = Date.now().toExponential();
   return (
-    <div className='flex justify-center md:mx-2 h-96 border-t-4 bg-gradient-to-t from-gray-500 to-white mt-1'>
-
-          <div className='flex justify-around  w-40 md:w-96 md:h-20 h-12 my-auto text-2xl md:text-6xl sm:text-white md:text-blue-500'>
-           <Link><BiLogoFacebookCircle /></Link>
-              <Link><BsWhatsapp /></Link>
-             <Link><BsLinkedin /></Link>
-            <Link><BsInstagram /></Link>
-              <Link><BsGithub /></Link>
-            </div>
-            <div className='absolute bottom-0 border'>
-              <p className='p-2'><span className='inline-block'><AiOutlineCopyright/></span> DMSolutions:{date}</p>
-            </div>
+    <div className="flex justify-center md:mx-2 h-96 border-t-4 bg-gradient-to-t from-gray-500 to-white mt-1">
+      <div className="flex justify-around  w-40 md:w-96 md:h-20 h-12 my-auto text-2xl md:text-6xl sm:text-white md:text-blue-500">
+        <a href="#" target="_blank" className="">
+          <BiLogoFacebookCircle />
+        </a>
+        <a href="#" target="_blank" className="">
+          <BsWhatsapp />
+        </a>
+        <a href="#" target="_blank" className="">
+          <BsLinkedin />
+        </a>
+        <a href="#" target="_blank" className="">
+          <BsInstagram />
+        </a>
+        <a href="#" target="_blank" className="">
+          <BsGithub />
+        </a>
+      </div>
+      <div className="absolute bottom-0 border">
+        <p className="p-2">
+          <span className="inline-block">
+            <AiOutlineCopyright />
+          </span>{" "}
+          DMSolutions:{date}
+        </p>
+      </div>
     </div>
+  );
+};
 
-
-  )
-}
-
-export default Footer
+export default Footer;
