d11cc231ce7d2fd8999de0992490bce710bf5c80
David <david.owuor@student.moringaschool.com>
2023-10-04 23:57:03 -0500

    fields

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -4,7 +4,35 @@ const Signup = () => {
   return (
     <div>
         <form>
+            <div>
+                <h2>Personal Details</h2>
             <input type="text" />
+            <input type="text" />
+            <input type="text" />
+            <input type="text" />
+            </div>
+            <div>
+                <h2>Personal Details</h2>
+            <input type="text" />
+            <input type="text" />
+            <input type="text" />
+            <input type="text" />
+            </div>
+            <div>
+                <h2>Personal Details</h2>
+            <input type="text" />
+            <input type="text" />
+            <input type="text" />
+            <input type="text" />
+            </div>
+            <div>
+                <h2>Personal Details</h2>
+            <input type="text" />
+            <input type="text" />
+            <input type="text" />
+            <input type="text" />
+            </div>
+            <button type="button">Submit</button>
         </form>
     </div>
   )
